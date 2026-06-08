6b8ea872ddf6de42ec7dd72be6971b050c156dde
TrianguloY <correo--correo@hotmail.com>
2026-06-08 23:41:14 +0200

    feat(thinkyDailies): replaced bruteforce approach with a hierarchical search

--- thinkyDailies/betaImages.py
+++ thinkyDailies/betaImages.py
@@ -9,18 +9,35 @@ import json
 import platform
 import random
 from asyncio import sleep
-from typing import List, Tuple, Optional
+from typing import List, Optional, Union, Iterable, NamedTuple
 
 import aiohttp
 from aiohttp import ClientSession
 
-INFIXES: List[str] = [
-    # base
-    "",
-    # known
-    "-1", "-2", "a", "%20(1)", "%20(2)", "a%20(1)", "A",
-    # extra just in case
-    "-3", "b", "%20(3)", "b%20(1)", "a%20(2)", "B",
+
+class Entry(NamedTuple):
+    season: int
+    puzzle: int
+    prefix: str
+    suffix: str
+
+
+class Result(NamedTuple):
+    season: str
+    puzzle: str
+    infix: str
+    url: str
+
+
+InfixNode = Union[str, List["InfixNode"]]
+
+# If str, no more elements in the list will be checked.
+INFIXES: List[InfixNode] = [
+    [""],  # sometimes this may not exist, we allow failing
+    ["-1", "-2", "-3"],
+    ["%20(1)", "%20(2)", "%20(3)"],
+    ["a", ["a%20(1)", "a%20(2)"], ["b", ["b%20(1)"], "c"]],
+    ["A", "B"],
 ]
 
 
@@ -32,56 +49,90 @@ def main() -> None:
     """Main."""
 
     print("Building entries")
-    entries = build_entries()
-    valid_entries = asyncio.run(filter_entries(entries))
+    entries: List[Entry] = []
+    for season in [1, 2, 3]:
+        for puzzle in range(1, 61 + 1):
+            entries.append(Entry(season,
+                                 puzzle,
+                                 f"https://static.prod.thinkygames.com/uploads/puzzle_intro_images/{season}x{puzzle:02}",
+                                 "-600x600.png"))
+
+    print("Requesting")
+    results = asyncio.run(parse_entries(entries))
 
     print("Statistics:")
-    for infix in INFIXES:
-        print(f"Infix '{infix}' has {sum(infix == i for _s, _p, i, _u in valid_entries)} elements")
+
+    def flatten(xs):
+        for x in xs:
+            if isinstance(x, Iterable) and not isinstance(x, (str, bytes)):
+                yield from flatten(x)
+            else:
+                yield x
+
+    for infix in flatten(INFIXES):
+        print(f"Infix '{infix}' has {sum(infix == result.infix for result in results)} elements")
 
     print("Writing to file")
     images = {}
-    for season, puzzle, _, url in valid_entries:
-        images.setdefault(season, {}).setdefault(puzzle, []).append(url)
-    with open("betaImages.json", 'r+') as output:
-        data = json.load(output)
-        data['images'] = images
-        output.seek(0)
-        json.dump(data, output, indent=2)
+    for result in results:
+        images.setdefault(result.season, {}).setdefault(result.puzzle, []).append(result.url)
+    with open("betaImages.json") as file:
+        data = json.load(file)
+    data['images'] = images
+    with open("betaImages.json", 'w') as file:
+        json.dump(data, file, indent=2)
 
     print("done")
 
 
-def build_entries() -> List[Tuple[int, int, str, str]]:
-    """Builds all possible entries (a bit of a brute-force approach)"""
-    data: List[Tuple[int, int, str, str]] = []
-
-    for season in [1, 2, 3]:
-        for puzzle in range(1, 61 + 1):
-            for infix in INFIXES:
-                data.append((season, puzzle, infix,
-                             f"https://static.prod.thinkygames.com/uploads/puzzle_intro_images/{season}x{puzzle:02}{infix}-600x600.png"))
-    return data
-
-
-async def filter_entries(entries: List[Tuple[int, int, str, str]]) -> List[Tuple[int, int, str, str]]:
+async def parse_entries(entries: List[Entry]) -> List[Result]:
     """Removes invalid entries (where the url doesn't exist)."""
     async with aiohttp.ClientSession() as session:
-        filtered = await asyncio.gather(*(keep_if_exists(entry, session) for entry in entries))
-    return [entry for entry in filtered if entry is not None]
+        filtered = await asyncio.gather(*(parse_entry(entry, session) for entry in entries))
+    return [entry for result in filtered for entry in result]
 
 
-async def keep_if_exists(entry: Tuple[int, int, str, str], session: ClientSession) -> Optional[
-    Tuple[int, int, str, str]]:
-    """Returns the entry if it exists (based on a HEAD request), None if not."""
+async def parse_entry(entry: Entry, session: ClientSession) -> List[Result]:
+    """Parses an entry with all infixes."""
+    return await parse_infix(INFIXES, entry, session)
+
+
+async def parse_infix(infix: List[InfixNode], entry: Entry, session: ClientSession) -> List[Result]:
+    """Parses an entry with a list of infixes."""
+    results: List[Result] = []
+    for infx in infix:
+        if isinstance(infx, str):
+            # single value
+            result = await keep_if_exists(
+                Result(
+                    season=str(entry.season),
+                    puzzle=str(entry.puzzle),
+                    infix=infx,
+                    url=entry.prefix + infx + entry.suffix,
+                ),
+                session,
+            )
+            if result is not None:
+                results.append(result)
+            else:
+                # if a value fails, stop evaluating
+                break
+        else:
+            # list, continue regardless
+            results.extend(await parse_infix(infx, entry, session))
+    return results
+
+
+async def keep_if_exists(result: Result, session: ClientSession) -> Optional[Result]:
+    """Returns the entry if it exists (based on a HEAD request), None if not. Retries on network error"""
     exc = None
     for retry in range(1, 10 + 1):
         try:
             await sleep(random.random() * retry)
-            async with session.head(url=entry[3]) as response:
+            async with session.head(url=result.url) as response:
                 ok = response.ok
-                print("OK" if ok else "ko", entry[3])
-                return entry if ok else None
+                print("OK" if ok else "ko", result.url)
+                return result if ok else None
         except Exception as e:
             exc = e
             await sleep(random.randint(1, retry))
